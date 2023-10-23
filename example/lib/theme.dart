cffb4df77282eada0064a9a8a4742fc7da6d9476
useEffects <joel.sr1024@gmail.com>
2023-10-23 17:44:03 +0530

    add example

---
+++ example/lib/theme.dart
@@ -0,0 +1,36 @@
+import 'package:catppuccin_flutter/catppuccin.dart';
+import 'package:flutter/material.dart';
+
+ThemeData catppuccinTheme() {
+  Flavor flavor = ctp.mocha;
+  Color primaryColor = flavor.mauve;
+  Color secondaryColor = flavor.pink;
+  return ThemeData(
+      useMaterial3: true,
+      appBarTheme: AppBarTheme(
+          elevation: 0,
+          titleTextStyle: TextStyle(
+              color: flavor.text, fontSize: 20, fontWeight: FontWeight.bold),
+          backgroundColor: flavor.crust,
+          foregroundColor: flavor.mantle),
+      colorScheme: ColorScheme(
+        background: flavor.base,
+        brightness: Brightness.light,
+        error: flavor.surface2,
+        onBackground: flavor.text,
+        onError: flavor.red,
+        onPrimary: primaryColor,
+        onSecondary: secondaryColor,
+        onSurface: flavor.text,
+        primary: flavor.crust,
+        secondary: flavor.mantle,
+        surface: flavor.surface0,
+      ),
+      textTheme: const TextTheme().apply(
+        bodyColor: flavor.text,
+        displayColor: primaryColor,
+      ),
+      floatingActionButtonTheme: const FloatingActionButtonThemeData(
+        elevation: 0,
+      ));
+}
